d728946c91d94a560a297f981c28b3b7a37c5f39
Pedro de Medeiros <pedro.medeiros@gmail.com>
2021-05-03 10:49:22 -0300

    Endereço ip no script de criação de containers

--- docker/create_containers.sh
+++ docker/create_containers.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-docker-compose build
+
+# Removendo os containers velhos
+docker stop docker_mysql-server_1 docker_phpmyadmin_1 docker_web-server_1
+docker rm docker_mysql-server_1 docker_phpmyadmin_1 docker_web-server_1
+
+# Criandos containers novos
+docker-compose up -d --build
