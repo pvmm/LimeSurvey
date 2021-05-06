25cf7f31150933b0fbe655e59521659f01612d04
Pedro de Medeiros <pedro.medeiros@gmail.com>
2021-05-06 12:00:58 -0300

    bloqueando containeres para acesso externo

--- docker/create_containers.sh
+++ docker/create_containers.sh
@@ -5,4 +5,5 @@ docker stop docker_mysql-server_1 docker_phpmyadmin_1 docker_web-server_1
 docker rm docker_mysql-server_1 docker_phpmyadmin_1 docker_web-server_1
 docker rmi docker_mysql-server docker_phpmyadmin docker_web-server
 
+# Criandos containers novos
 docker-compose build
